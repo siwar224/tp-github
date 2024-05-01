bed444f4e7bf50163727970e6c25eb74b5172097
siwar224 <siwarmsellem@gmail.com>
2024-05-01 15:31:48 +0200

    Structuration

--- src/main/java/resources/Calculatrice.java
+++ src/main/java/resources/Calculatrice.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel; // affichage
+import javax.swing.JLabel; 
 import javax.swing.JPanel;
 
 
